77cf7b457fed57323eb0cdcc59f58359ec246066
manuel_perez <manel.aguila.1606@gmail.com>
2025-09-04 06:10:41 -0600

    Se añade descripción al script euler_p4.py, falta documentar/modificar

--- euler_p4.py
+++ euler_p4.py
@@ -1,3 +1,6 @@
+# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91x99.
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
 def invertidor(cadena):
     reverso = ""
     i = len(cadena)
