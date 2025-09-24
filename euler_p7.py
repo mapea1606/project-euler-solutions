9c1cbacb683aa39770e0e7f0f7ad15ff811fae39
manuel_perez <manel.aguila.1606@gmail.com>
2025-09-24 02:28:57 -0600

    Se añade descripción del script euler_p7.py

--- euler_p7.py
+++ euler_p7.py
@@ -1,3 +1,7 @@
+# Problem 7
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+# What is the 10,001 st prime number?
+
 def es_primo(num):
     i = 2
     while i < num:
