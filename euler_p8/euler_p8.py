ac82e47ebeed87ab60b4df1d1084147fb32ffd95
manuel_perez <manel.aguila.1606@gmail.com>
2025-11-04 22:51:35 -0600

    Se ha añadido descripción al programa euler_p8.py.

--- euler_p8/euler_p8.py
+++ euler_p8/euler_p8.py
@@ -1,3 +1,9 @@
+# Problem 8
+
+# The four adjacent digits in the 1000-digit number that have the greatest product are (9)(9)(8)(9) = 5832.
+# Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
+# You can see the number in the euler_p8.txt file.
+
 def multiplicador(cadena):
     i = 0
     p = 1
